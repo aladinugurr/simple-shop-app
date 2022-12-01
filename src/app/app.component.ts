083d42b90cdac5465e0ca63e3bc1ecb42e112219
aladinugurr <ugur.catal8@gmail.com>
2022-12-01 19:39:28 +0300

    initial  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {products} from './products';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+    title = "shop";
+
+    // Ürünler Listesi
+  products = products;
+  //Sepet
+  basket : Product[] = [];
+  // Sepet Fiyatı Hesaplama 
+  getTotal() : string {
+    let total = 0;
+
+    for(let item of this.basket){
+      total = total + (item.price * item.quantity)
+    }
+    return total.toFixed(2) + " TL";
+  }
+
+  decreaseAmount(product : Product) : void {
+    // Ürün miktarı 0 ise bir şey yapma
+    if(product.quantity == 0){
+      return;
+    }
+
+    // Ürün miktarını azalt
+    product.quantity--;
+  
+    //Ürün miktarı 0 olursa sepetten çıkart
+    if(product.quantity == 0){
+      let index = this.basket.indexOf(product);
+      this.basket.splice(index, 1);
+    }
+  }
+  increaseAmount(product : Product) : void {
+      // Sepette yoksa ekle
+      if(!this.basket.includes(product)){
+        this.basket.push(product)
+      }
+      // Ürün miktarını arttır
+      product.quantity++;
+    
+  }
+  removeIfZero(product : Product) : void {
+    if(product.quantity == 0){
+
+      let index = this.basket.indexOf(product);
+      this.basket.splice(index, 1);
+    }
+    
+    }
+    addIfNotBasket(product : Product) : void {
+      if(!this.basket.includes(product) && product.quantity > 0){
+
+        this.basket.push(product);
+      }
+    }
+    updateBasket(product : Product) : void {
+      // Ürün miktarı 0 olursa sepetten çıkart
+      this.removeIfZero(product);    
+      // Sepette yoksa ekle
+      this.addIfNotBasket(product);
+    }
+  }
+
+type Product = {
+  id : number,
+  name : string,
+  photoPath : string,
+  price : number,
+  unit : string,
+  quantity : number
+
 }
---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+    {
+        id : 1,
+        name : 'Domates',
+        photoPath : "assets/images/domates.jpeg",
+        price : 3.99,
+        unit : 'Kg',
+        quantity : 0
+    },
+    {
+        id : 2,
+        name : 'Çipura',
+        photoPath : 'assets/images/cipura.jpeg',
+        price : 14,
+        unit : 'Adet',
+        quantity : 0
+    },
+    {
+        id : 3,
+        name : 'Sek Süt 1L',
+        photoPath : 'assets/images/sek.jpeg',
+        price : 8.2,
+        unit : 'Adet',
+        quantity : 0
+    },
+    {
+        id : 4,
+        name : 'Pastavilla Burgu Makarna 500G',
+        photoPath : 'assets/images/makarna.jpg',
+        price : 5.25,
+        unit : 'Adet',
+        quantity : 0
+    },
+    {
+        id : 5,
+        name : 'Coca-Cola Zero Sugar 1 L',
+        photoPath : 'assets/images/kola.jpeg',
+        price : 4.95,
+        unit : 'Adet',
+        quantity : 0
+    },
+    {
+        id : 6,
+        name : 'Solo Havlu 12\'Li',
+        photoPath : 'assets/images/solo.jpeg',
+        price : 43.5,
+        unit : 'Adet',
+        quantity : 0
+    },
+];
+
